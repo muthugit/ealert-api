2397495f5518357625291ffbb7c2dd2a71a41793
Muthu Pandian <contact@muthupandian.in>
2016-09-19 00:35:20 +0530

    API

--- index.js
+++ index.js
@@ -11,23 +11,27 @@ app.use(bodyParser.urlencoded({
 	limit : '50mb',
 	extended : true
 }));
-
+// DEMO
 app.use(bodyParser.urlencoded({
 	extended : true
 }));
 // END BODY PARSER
 
-app.post('/', function (req, res) {
-	//res.send('Hello World!');
-	var userName=req.body.user_name;
-	var channelName=req.body.channel_name;
-	var text=req.body.text;
-	var triggeredWord=req.body.trigger_word;
-	var domain=req.body.team_domain;
+app.post('/', function(req, res) {
+	// res.send('Hello World!');
+	var userName = req.body.user_name;
+	var channelName = req.body.channel_name;
+	var text = req.body.text;
+	var triggeredWord = req.body.trigger_word;
+	text = text.replace(triggeredWord, "");
+	var domain = req.body.team_domain;
 	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({ "text": "Hi: "+userName+" you requested from: "+channelName+" of "+domain }));
+	res.send(JSON.stringify({
+		"text" : "Hi: " + userName + " you requested from: " + channelName
+				+ " of " + domain + " and your message is: " + text
+	}));
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
