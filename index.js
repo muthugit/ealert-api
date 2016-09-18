189ffdd16f12d33d6e0093f94954992999b0a8f1
Muthu <contact@muthupandian.in>
2016-09-18 17:48:00 +0000

    API

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
+// BODY PARSER
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({
+	limit : '50mb',
+	extended : true
+}));
+app.use(bodyParser.urlencoded({
+	limit : '50mb',
+	extended : true
+}));
 
-app.get('/', function (req, res) {
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+// END BODY PARSER
+
+app.post('/', function (req, res) {
   //res.send('Hello World!');
+var userName=req.body.user_name;
  res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 2 }));
+    res.send(JSON.stringify({ "text": "Hi: "+userName+" i am muthu" }));
 });
 
 app.listen(3000, function () {
