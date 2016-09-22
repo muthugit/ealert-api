006482d513daf1b4701d72dd40830a2addcbaebb
Muthu Pandian <contact@muthupandian.in>
2016-09-23 01:59:41 +0530

    API

--- message.js
+++ message.js
@@ -17,7 +17,8 @@ var messageRepository = function() {
         if (results.length > 0) {
           console.log("Channel exists");
         } else {
-          var channelObject=[{'channelName':channelName,'organization':organization}];
+          var channelObject=[];
+          channelObject=[{'channelName':channelName,'organization':organization}];
           self.createSingleObject(Parse,channelObject,"channels");
         }
       }
